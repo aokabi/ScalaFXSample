dff86aae373ae20cf5d531fd06695dbb8488ebc8
aokabi <aokabit@gmail.com>
2017-07-19 07:39:55 +0900

    テキストの色とかサイズとか変更

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,12 @@
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
 import scalafx.scene.text.Text
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint.{LinearGradient, Stops}
 
 /**
   * Created by aokabi on 2017/07/19.
@@ -11,11 +15,32 @@ object HelloWorld extends JFXApp {
   stage = new PrimaryStage {
     title = "ScalaFX Hello World"
     scene = new Scene {
-      root = new VBox {
-        children = new Text {
+      fill = Black
+      content = new HBox {
+        padding = Insets(20)
+        children = Seq(
+          new Text {
             text = "Hello"
-          
-        }
+            style = "-fx-font-size: 48pt"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(PaleGreen, SeaGreen)
+            )
+        },
+          new Text {
+            text = "World!"
+            style = "-fx-font-size: 48pt"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(Cyan, DodgerBlue)
+            )
+            effect = new DropShadow {
+              color = DodgerBlue
+              radius = 25
+              spread = 0.25
+            }
+          }
+        )
       }
     }
   }
